fefdc1f38530be31dbf6896573290acc090ae744
slashssab <slashssab@gmail.com>
2017-04-14 22:01:52 +0200

    Add files via upload

---
+++ Health.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour
+{
+    public const int maxHealth = 100;
+
+    [SyncVar(hook = "OnChangeHealth")]    
+    public int currentHealth = maxHealth;
+    public RectTransform healthBar;
+
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer)
+        {
+            return;
+        }
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Dead!");
+        }
+    
+    }
+    void OnChangeHealth(int health)
+    {
+        healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
+    }
+}
